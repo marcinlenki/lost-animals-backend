e6ee7d43c5907014355242915727538c26cc23ef
mikpas138_dpp_2023 <mikpas138_dpp_2023@e-science.pl>
2023-05-10 10:13:12 +0200

    fixed pull request
    delete SwaggerConfig.java and fixed run.sh

--- backend-environment-configuration/run.sh
+++ backend-environment-configuration/run.sh
@@ -6,13 +6,14 @@ if [ $# -eq 0 ]; then
 else
   docker_profile=$1
 
-  if [ ! "${docker_profile}" == "frontend" ] || [ ! "${docker_profile}" == "backend" ]; then
+  if [[ ! "$docker_profile" =~ ^(frontend|backend)$ ]]; then
         echo "Invalid profile ${docker_profile}!"
         exit 1
   fi
 fi
 
-docker-compose stop
-docker-compose rm -f
-docker-compose pull
-docker-compose --profile "${docker_profile}" up -d
+docker-compose stop &&
+docker-compose rm -f &&
+docker-compose pull &&
+docker-compose --profile "${docker_profile}" up -d &&
+echo "Finished container initilization..."
--- src/main/java/pl/edu/pwr/kpz/lostanimalsbackend/config/SwaggerConfig.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.pwr.kpz.lostanimalsbackend.config;
-
-public class SwaggerConfig {
-
-
-}
