d1c09407120328358e54e152973aceeeaec4c0a5
mikpas138_dpp_2023 <mikpas138_dpp_2023@e-science.pl>
2023-05-21 19:22:21 +0200

    delete ignore setId by mapper

--- src/main/java/pl/edu/pwr/kpz/lostanimalsbackend/config/MappingConfig.java
+++ src/main/java/pl/edu/pwr/kpz/lostanimalsbackend/config/MappingConfig.java
@@ -4,8 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.edu.pwr.kpz.lostanimalsbackend.model.dto.UserRequestDTO;
-import pl.edu.pwr.kpz.lostanimalsbackend.model.entities.User;
 
 @Configuration
 public class MappingConfig {
@@ -15,8 +13,6 @@ public class MappingConfig {
         modelMapper
                 .getConfiguration().setAmbiguityIgnored(true)
                 .setMatchingStrategy(MatchingStrategies.STANDARD);
-        modelMapper.createTypeMap(UserRequestDTO.class, User.class)
-                .addMappings(mapping->mapping.skip(User::setId));
         return modelMapper;
     }
 }
