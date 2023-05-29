27bca098a308c06a896abb29f365c58812d02b91
Marcin Lenkiewicz <marlen221_dpp_2023@e-science.pl>
2023-05-29 20:57:31 +0200

    Added git to the script

--- backend-environment-configuration/run-arm.sh
+++ backend-environment-configuration/run-arm.sh
@@ -3,9 +3,10 @@
 env_file=".env"
 compose_file="docker-compose-arm.yml"
 mvn="../mvnw" # use maven wrapper
+current_branch=$(git branch --show-current)
 
 if ! docker info > /dev/null 2>&1; then
-  echo "ERROR: Docker isn't running, please start Docker and try again! Exiting..."
+  echo "[ERROR]: Docker isn't running, please start Docker and try again! Exiting..."
   exit 1
 fi
 
@@ -16,7 +17,7 @@ else
   docker_profile=$1
 
   if [[ ! "$docker_profile" =~ ^(frontend|backend)$ ]]; then
-        echo "ERROR: Invalid profile ${docker_profile}! Exiting..."
+        echo "[ERROR]: Invalid profile ${docker_profile}! Exiting..."
         exit 1
   fi
 fi
@@ -26,18 +27,32 @@ if [[ "$docker_profile" = "backend" ]]; then
   touch "$env_file"
 
 elif [ "$docker_profile" = "frontend" ] && [ ! -f "${env_file}" ]; then
-  echo "ERROR: Can't locate \"$env_file\" environment file in the current directory! You should manually add the file. Exiting..."
+  echo "[ERROR]: Can't locate \"$env_file\" environment file in the current directory! You should manually add the file. Exiting..."
   exit 1
 fi
 
+echo "[INFO] Checking for changes in the remote..."
+git diff "$current_branch..origin/$current_branch" --quiet;
+change=$?
+
+if [ "$change" == 1 ]; then
+  echo -n "[INFO] Changed detected, pulling from the remote... "
+  git pull --quiet
+  echo "Done"
+  rm -rf "../target"
+fi
 
 if ! compgen -G "../target/*.jar" > /dev/null; then
-    echo "Building Spring Boot Application..."
+    echo -n "[INFO] Building Spring Boot Application... "
 
     if ! eval "$mvn -B package --file ../pom.xml -DskipTests > /dev/null"; then
-      echo "Unable to build Spring project. Exiting..."
+      echo "" #newline
+      echo "[ERROR]: Unable to build Spring project. Exiting..."
       exit 1
+    else
+      echo "Done"
     fi
+
 fi
 
 docker-compose --profile "${docker_profile}" -f "$compose_file" stop &&
